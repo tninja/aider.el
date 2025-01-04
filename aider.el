bebf6786e53e39a79d71836bac057e43ad11c41a
tninja <tninja@gmail.com>
2025-01-04 10:06:10 -0800

    refactor(keybind): change go ahead key from g to y

--- aider.el
+++ aider.el
@@ -128,7 +128,7 @@ Affects the system message too.")
     ]
    ["Discussion"
     ("q" "Ask Question" aider-ask-question)
-    ("g" "Go Ahead" aider-go-ahead)
+    ("y" "Go Ahead" aider-go-ahead)
     ("e" "Explain Function or Region" aider-function-or-region-explain)
     ("p" "Explain Symbol Under Point" aider-explain-symbol-under-point)
     ("D" "Debug Exception" aider-debug-exception)
