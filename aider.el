ebcba531f897605a2aca066888feb93351d26617
tninja <tninja@gmail.com>
2025-03-19 21:53:23 -0700

    style: remap command keys for better ergonomics

--- aider.el
+++ aider.el
@@ -84,7 +84,7 @@ Also based on aider LLM benchmark: https://aider.chat/docs/leaderboards/"
     ("f" "Add Current / Marked File (C-u: readonly)" aider-add-current-file-or-dired-marked-files)
     ("w" "Add All Files in Window" aider-add-files-in-current-window)
     ("d" "Add Same Type Files in dir" aider-add-same-type-files-under-dir)
-    ("O" "Drop Current File" aider-drop-current-file)
+    ;; ("O" "Drop Current File" aider-drop-current-file)
     ("m" "Show Last Commit (C-u: magit-log)" aider-magit-show-last-commit-or-log)
     ("u" "Undo Last Change" aider-undo-last-change)
     ("v" "Pull or Review Code Change" aider-pull-or-review-diff-file)
@@ -101,7 +101,7 @@ Also based on aider LLM benchmark: https://aider.chat/docs/leaderboards/"
     ("q" "Question on Function / Region" aider-ask-question)
     ("y" "Then Go Ahead" aider-go-ahead)
     ("Q" "Question without Context" aider-general-question)
-    ("D" "Debug Exception" aider-debug-exception)
+    ("e" "Debug Exception" aider-debug-exception)
     ("h" "Help (C-u: homepage)" aider-help)
     ("x" "Exit Aider" aider-exit)
     ]
