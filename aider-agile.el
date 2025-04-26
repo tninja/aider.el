729c078c568928feeecfbb807e304e2ce7cb2e58
tninja <tninja@gmail.com>
2025-04-26 09:49:52 -0700

    refactor: Use aider-current-file-command-and-switch for /ask command

--- aider-agile.el
+++ aider-agile.el
@@ -176,9 +176,7 @@
                          ""))
          (prompt (format "Analyze the following code context and suggest appropriate refactoring strategy.\nContext: %s%s\n"
                          context-info
-                         code-snippet))
-         (command (format "/ask \"%s\"" prompt)))
-    (aider--send-command command t) ;; Use aider--send-command for /ask
+                         code-snippet)))
     (aider-current-file-command-and-switch "/ask " prompt)
     (message "Requesting refactoring suggestion from Aider. If you are happy with the suggestion, use 'go ahead' to accept the change")))
 
