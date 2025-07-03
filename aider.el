39fa9f5cc3c1e7460ad8c0fa37e4ccc2f2f411f4
tninja <tninja@gmail.com>
2025-07-03 06:34:42 -0700

    update menu item short key

--- aider.el
+++ aider.el
@@ -133,8 +133,8 @@ Also based on aider LLM benchmark: https://aider.chat/docs/leaderboards/"
   ("C" "Copy To Clipboard"          aider-copy-to-clipboard)
   ("P" "Software Planning"          aider-start-software-planning)
   ("E" "Debug Exception"            aider-debug-exception)
-  ("R" "Run Current File"           aider-run-current-file)
   ("h" "Open History"               aider-open-history)
+  ("!" "Run Current File"           aider-run-current-file)
   ("?" "Help (C-u: homepage)"       aider-help))
 
 ;; The instruction in the autoload comment is needed, see
